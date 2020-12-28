c6d00eaf11e4f684e99e7c53b0c843d32e555269
Serhii Savychenko <glogrus@gmail.com>
2020-12-28 23:14:56 +0200

    Add Skip Search Algorithm.

---
+++ src/ESMA/Algorithms/SkipSearch.cs
@@ -0,0 +1,93 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SkipSearch.cs" company="GLogrus">
+//   Copyright (c) GLogrus. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ESMA.Algorithms
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The Skip Search.
+    /// </summary>
+    [Algorithm("Skip Search")]
+    public class SkipSearch : BaseMatch
+    {
+        /// <summary>
+        ///     The skip search list.
+        /// </summary>
+        private GenericList<int>.Node[] skipSearchList;
+
+        /// <summary>
+        /// The internal match.
+        /// </summary>
+        /// <param name="data">
+        /// The data.
+        /// </param>
+        /// <param name="indexes">
+        /// The indexes.
+        /// </param>
+        /// <param name="length">
+        /// The length.
+        /// </param>
+        /// <param name="offset">
+        /// The offset.
+        /// </param>
+        /// <returns>
+        /// The array of index.
+        /// </returns>
+        protected override int InternalMatch(byte[] data, List<long> indexes, int length, long offset = 0)
+        {
+            var pattern = this.Pattern;
+            var list = this.skipSearchList;
+
+            var i = pattern.Length - 1;
+            for (; i < length; i += this.Pattern.Length)
+            {
+                for (var node = list[data[i]]; node != null; node = node.Next)
+                {
+                    int j;
+                    for (j = 0; j < pattern.Length; j++)
+                    {
+                        var index = i + j - node.Data;
+                        if (index >= data.Length)
+                        {
+                            break;
+                        }
+
+                        if (pattern[j] != data[index])
+                        {
+                            break;
+                        }
+                    }
+
+                    if (j >= pattern.Length)
+                    {
+                        indexes.Add(i - node.Data + offset);
+                    }
+                }
+            }
+
+            return i;
+        }
+
+        /// <summary>
+        ///     The prepare.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="bool" />.
+        /// </returns>
+        protected override bool Prepare()
+        {
+            this.skipSearchList = new GenericList<int>.Node[256];
+            for (var i = 0; i < this.Pattern.Length; i++)
+            {
+                this.skipSearchList[this.Pattern[i]] =
+                    new GenericList<int>.Node { Data = i, Next = this.skipSearchList[this.Pattern[i]] };
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/ESMA/GenericList.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GenericList.cs" company="GLogrus">
+//   Copyright (c) GLogrus. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ESMA
+{
+    /// <summary>
+    /// The generic list.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type of list items.
+    /// </typeparam>
+    public class GenericList<T>
+    {
+        /// <summary>
+        ///     The head.
+        /// </summary>
+        private Node head;
+
+        /// <summary>
+        /// The add node.
+        /// </summary>
+        /// <param name="node">
+        /// The node.
+        /// </param>
+        public void AddNode(T node)
+        {
+            var newNode = new Node { Next = this.head, Data = node };
+            this.head = newNode;
+        }
+
+        /// <summary>
+        ///     The get first added.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="T" />.
+        /// </returns>
+        public T GetFirstAdded()
+        {
+            // The value of temp is returned as the value of the method. 
+            // The following declaration initializes temp to the appropriate 
+            // default value for type T. The default value is returned if the 
+            // list is empty.
+            var temp = default(T);
+
+            var current = this.head;
+            while (current != null)
+            {
+                temp = current.Data;
+                current = current.Next;
+            }
+
+            return temp;
+        }
+
+        /// <summary>
+        ///     The node.
+        /// </summary>
+        internal class Node
+        {
+            /// <summary>
+            ///     Gets or sets the data.
+            /// </summary>
+            public T Data { get; set; }
+
+            /// <summary>
+            ///     Gets or sets the next.
+            /// </summary>
+            public Node Next { get; set; }
+        }
+    }
+}
